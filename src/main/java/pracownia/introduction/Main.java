0c49c4beb8ff48f438ba3952827e001716d5c6e2
s434761 <ystro21@wp.pl>
2018-10-08 13:13:50 +0200

    sampleText

--- src/main/java/pracownia/introduction/Main.java
+++ src/main/java/pracownia/introduction/Main.java
@@ -1,8 +1,15 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+
+    static Logger log =  Logger.getLogger("asd");
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        log.info("MEssage");
     }
+
+
 }
